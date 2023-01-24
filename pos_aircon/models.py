2e49c0dd4716146d7dcaa395c826ab20e7b4b3fd
mochizombie <carylcium@gmail.com>
2023-01-25 00:20:09 +0800

    progres

--- pos_aircon/models.py
+++ pos_aircon/models.py
@@ -2,26 +2,49 @@ from django.db import models
 
 # Create your models here.
 
+# items
 class tools_inventory(models.Model):
     item_name = models.CharField(max_length=255)
     item_quantity = models.IntegerField()
     item_type = models.CharField(max_length=255, default=" ")
+    item_price = models.FloatField(default=0)
     
+# customer details
 class customer_details(models.Model):
     customerName = models.CharField(max_length=255)
     customerAddress = models.CharField(max_length=255)
     customerEmail = models.CharField(max_length=255)
     customerPhone = models.CharField(max_length=255)
-    customerStatus = models.BooleanField()
+    customerStatus = models.BooleanField(default=True)
+
     
+# techinician
 class technician_details(models.Model):
     techName = models.CharField(max_length=255)
     techPhone = models.CharField(max_length=255)
     techEmail = models.CharField(max_length=255)
     techSched = models.CharField(max_length=255)
 
+# supplier
 class supplier_details(models.Model):
     suppName = models.CharField(max_length=255)
     suppAddress = models.CharField(max_length=255)
     suppEmail = models.CharField(max_length=255)
     suppPhone = models.CharField(max_length=255)
+
+
+### payment methods
+### PAYMENT_MODES = (
+###     ('Cash', 'Cash'),
+###     ('Credit', 'Credit'),
+### )
+
+
+### class sales_order(models.Model):
+###     customer = models.ForeignKey(customer_details, on_delete=models.CASCADE)
+###     item = models.ForeignKey(tools_inventory, on_delete=models.CASCADE)
+###     dateOrdered = models.DateField(auto_now_add=True)
+###     paymentMethod = models.CharField(max_length=10, null=True, choices=PAYMENT_MODES)
+
+###    
+
--- pos_aircon/views.py
+++ pos_aircon/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from pos_aircon.models import customer_details
+from django.contrib import messages
 
 # Create your views here.
+
+def customerDisplay(request):
+    results=customer_details.objects.all()
+    return render(request, "index.html", {"customer_details":results})
+
+def newCustomer(request):
+    if request.method=="POST":
+        if request.POST.get('customerName') and request.POST.get('customerPhone') and request.POST.get('customerEmail') and request.POST.get('customerAddress'):
+            saveCustomer = customer_details()
+            saveCustomer.customerName=request.POST.get('customerName')
+            saveCustomer.customerPhone=request.POST.get('customerPhone')
+            saveCustomer.customerEmail=request.POST.get('customerEmail')
+            saveCustomer.customerAddress=request.POST.get('customerAddress')
+            saveCustomer.save()
+            messages.success(request, "Customer Record" + saveCustomer.customerName + "'s details is recorded!")
+            return render(request,"create.html")
+        else:
+            return render(request,"create.html")
